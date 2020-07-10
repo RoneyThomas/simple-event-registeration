9fed3c321c4885eca4f481d0a3ea638defa33d0e
RoneyThomas <roneythomas6@gmail.com>
2020-07-10 00:50:50 -0400

    Proof of concept for event registration.

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,48 @@
-import Head from 'next/head'
+import Layout from '../components/layout'
+import styles from './index.module.scss'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import React, { Component } from 'react'
+import Router from 'next/router'
+import Button from '@material-ui/core/Button';
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export default class extends Component {
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+render() {
+return (
+  <Layout>
+    <section className="section">
+      <h2>Dearly beloved in XYZ,</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis diam a tincidunt posuere. Praesent scelerisque urna augue, a rhoncus leo hendrerit id. Aenean rhoncus euismod augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam laoreet urna quis urna finibus, sit amet iaculis augue tempus. Vivamus eleifend nulla imperdiet interdum egestas. Fusce enim ipsum, maximus non ultricies non, malesuada et lacus. Duis viverra cursus cursus.
+<ul>
+  <li>Tested positive for COVID-19 in the last 14 days.</li>
+  <li>Exposed to anyone positive with COVID-19 in the last 14 days.</li>
+  <li>Traveled outside Canada in the last 14 days.</li>
+  <li>In the past 14 days, you or any members of your household experienced any of the following:</li>
+  <ul>
+    <li>Fever (Temperature above 37.2ºC)</li>
+    <li>Cough</li>
+    <li>Difficulty breathing</li>
+    <li>Shortness of breath</li>
+    <li>Sore throat</li>
+    <li>Diarrhea</li>
+    <li>Body aches</li>
+    <li>Loss of taste or smell</li>
+  </ul>
+</ul>
+<div className={styles.covidDisclamirButton}>
+<Button variant="contained" color="primary" onClick={() => Router.push('/disclaimer')}>
+  YES
+</Button>
+<div className={styles.spaceBetween}></div>
+<Button variant="contained" color="secondary" onClick={() => Router.push('/registration')}>
+  No
+</Button>
+</div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+</p>
+    </section>
+  </Layout>
+);
+}
 }
